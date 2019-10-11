b4dcdf2758e21b502239be70d51981358b501d12
willfarley <farley_w_@hotmail.com>
2019-10-11 11:05:48 +0100

    fix security group

--- security_groups.tf
+++ security_groups.tf
@@ -4,26 +4,18 @@ resource "aws_security_group" "application" {
   name        = "application-ec2-${var.application_tag_environment}"
   description = "Security Group for the application EC2 Instance"
   vpc_id      = "${var.application_vpc_id}"
-}
 
-# Allow application 22 ingress from trusted Bastion
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    security_groups   = ["${var.bastion_sg}"]
+  }
 
-resource "aws_security_group_rule" "application_allow_ssh_ingress_trusted" {
-  security_group_id = "${aws_security_group.application.id}"
-  type              = "ingress"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp"
-  security_groups   = ["${var.bastion_sg}"]
-}
-
-# Allow application egress for all
-
-resource "aws_security_group_rule" "application_allow_egress_all" {
-  security_group_id = "${aws_security_group.application.id}"
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
 }
