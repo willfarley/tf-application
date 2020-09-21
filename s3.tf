ba1e3789883d5c660c055d6f89e4f67a31507178
willfarley <farley_w_@hotmail.com>
2020-09-21 12:28:56 +0100

    Delete s3.tf

--- s3.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_s3_bucket" "bucket" {
-  bucket = "${var.application_tag_environment}-application"
-  acl    = "private"
-
-  tags = {
-    Name        = "${var.application_tag_environment}-application"
-    Environment = "${var.application_tag_environment}"
-  }
-}
